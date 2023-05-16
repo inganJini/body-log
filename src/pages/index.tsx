4781fd2e56583f2021398f7b29fb93dafd755c21
inganJini <doublevaluable@gmail.com>
2023-05-17 08:22:44 +0900

    React Study Day5 * localhost:3000 으로 홈 진입시 console.log 두번 호출되는 현상 발견

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,45 @@
 import {Inter} from 'next/font/google'
 import Link from "next/link";
+import {useEffect} from "react";
 
 const inter = Inter({subsets: ['latin']})
+const WORKOUTS = 'workouts';
 
-console.log(localStorage.getItem('workouts'))
+function getWorkoutList() {
+  const workouts = localStorage.getItem(WORKOUTS);
+  if (workouts !== null) {
+    const parsedWorkouts = JSON.parse(workouts) as string[];
+    const register = document.getElementById('register');
+    parsedWorkouts.forEach((workout) => {
+      const li = document.createElement('li');
+      li.innerText = workout;
+      if (register !== null) {
+        register.appendChild(li)
+      }
+    })
+  }
+}
 
 export default function Home() {
+  useEffect(() => {
+    console.log(localStorage.getItem(WORKOUTS))
+    if (localStorage.getItem(WORKOUTS) !== null) {
+      getWorkoutList();
+    }
+  }, [])
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <li className="flex-1">
+        <div className="flex-1">
           오늘 할 운동
-        </li>
-        <li className="flex-1">
+        </div>
+        <div className="flex-1" id="register">
           <Link href="/workout/create">
             등록
           </Link>
-        </li>
+        </div>
       </div>
     </main>
   )
