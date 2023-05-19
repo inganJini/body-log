314cb36707ae853f4ab078d63ba6508e2590119f
inganJini <doublevaluable@gmail.com>
2023-05-19 15:39:12 +0900

    Fix Day5 Hydration issue 보완

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,69 @@
 import {Inter} from 'next/font/google'
 import Link from "next/link";
-import {useEffect} from "react";
+import {useEffect, useMemo, useState} from "react";
 
 const inter = Inter({subsets: ['latin']})
 const WORKOUTS = 'workouts';
 
-function getWorkoutList() {
-  const workouts = localStorage.getItem(WORKOUTS);
-  if (workouts !== null) {
-    const parsedWorkouts = JSON.parse(workouts) as string[];
-    const register = document.getElementById('register');
-    parsedWorkouts.forEach((workout) => {
-      const li = document.createElement('li');
-      li.innerText = workout;
-      if (register !== null) {
-        register.appendChild(li)
-      }
-    })
-  }
-}
+// function getWorkoutList() {
+//   const workouts = localStorage.getItem(WORKOUTS);
+//   if (workouts !== null) {
+//     const parsedWorkouts = JSON.parse(workouts) as string[];
+//     const register = document.getElementById('register');
+//     parsedWorkouts.forEach((workout) => {
+//       const li = document.createElement('li');
+//       li.innerText = workout;
+//       if (register !== null) {
+//         register.appendChild(li)
+//       }
+//     })
+//   }
+// }
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
   useEffect(() => {
+    setMounted(true);
     console.log(localStorage.getItem(WORKOUTS))
-    if (localStorage.getItem(WORKOUTS) !== null) {
-      getWorkoutList();
+    // if (localStorage.getItem(WORKOUTS) !== null) {
+    //   getWorkoutList();
+    // }
+  }, []);
+
+  const workouts = useMemo(() => {
+    // Hydration issue. mounted 된 후에 localStorage에 접근해야함.
+    if (!mounted) return null;
+
+    try {
+      const strWorkouts = localStorage.getItem(WORKOUTS);
+      if (!strWorkouts) return null;
+
+      const parsedWorkouts = JSON.parse(strWorkouts) as string[];
+
+      return parsedWorkouts;
+    } catch (error) {
+      return null;
     }
-  }, [])
+  }, [mounted]);  // Dependency 중요!!
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex-1">
-          오늘 할 운동
-        </div>
-        <div className="flex-1" id="register">
-          <Link href="/workout/create">
-            등록
-          </Link>
-        </div>
+    <main>
+      <div>
+        오늘 할 운동
       </div>
+      <div id="register">
+        <Link href="/workout/create">
+          등록
+        </Link>
+      </div>
+      <ul>
+        {workouts && workouts.map((workout, index) => (
+          <li key={index}>
+            {workout}
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
